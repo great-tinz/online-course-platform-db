d8818800e761e42f0db3e1f954215e33163ddd8d
great-tinz <realemma343@gmail.com>
2026-08-01 21:35:23 +0000

    Add SQL queries for retrieving data from student, course, instructor, enrollment, review, and lesson tables

---
+++ queries/retrieval/select_all_queries.sql
@@ -0,0 +1,14 @@
+DESC student;
+DESC course;
+DESC instructor;
+DESC enrollment;
+DESC review;
+DESC lesson;
+
+
+SELECT * FROM student;
+SELECT * FROM course;
+SELECT * FROM instructor;
+SELECT * FROM enrollment;
+SELECT * FROM review;
+SELECT * FROM lesson;
